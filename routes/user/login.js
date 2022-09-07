899d1a90bac189a512aed42fed5c5bf2ffe0605b
ZiedChekir <ziedchekir1@gmail.com>
2022-09-07 18:09:46 +0100

    Update login.js

--- routes/user/login.js
+++ routes/user/login.js
@@ -1,44 +1,39 @@
-const User = require("../../models/User")
-const jwt = require('jsonwebtoken');
+const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
 
-module.exports = function(req, res) {
-    const { email, password } = req.body;
-    console.log("hiii")
-    User.findOne({ email }, function(err, user) {
-      if (err) {
-        console.error(err);
-        res.status(500)
-          .json({
-          error: 'Internal error please try again'
-        });
-      } else if (!user) {
-        res.status(401)
-          .json({
-            error: 'Incorrect email or password'
+module.exports = function (req, res) {
+  const { email, password } = req.body;
+  console.log("hiii");
+  User.findOne({ email }, function (err, user) {
+    if (err) {
+      console.error(err);
+      res.status(500).json({
+        error: "Internal error please try again",
+      });
+    } else if (!user) {
+      res.status(401).json({
+        error: "Incorrect email or password",
+      });
+    } else {
+      user.isCorrectPassword(password, function (err, same) {
+        if (err) {
+          res.status(500).json({
+            error: "Internal error please try again",
           });
-      } else {
-        user.isCorrectPassword(password, function(err, same) {
-          if (err) {
-            res.status(500)
-              .json({
-                error: 'Internal error please try again'
-            });
-          } else if (!same) {
-            res.status(401)
-              .json({
-                error: 'Incorrect email or password'
-            });
-          } else {
-            // Issue token
-            const payload = { email,id:user.id };
-            const token = jwt.sign(payload, process.env.AUTH_SECRET, {
-              expiresIn: '48h'
-            });
-            console.log(user.id)
-            res.cookie('token', token, { httpOnly: true })
-              .sendStatus(200);
-          }
-        });
-      }
-    });
-  }
+        } else if (!same) {
+          res.status(401).json({
+            error: "Incorrect email or password",
+          });
+        } else {
+          // Issue token
+          const payload = { email, id: user.id , first_name:user.first_name, last_name:user.last_name };
+          const token = jwt.sign(payload, process.env.AUTH_SECRET, {
+            expiresIn: "48h",
+          });
+          console.log(user.id);
+          res.cookie("token", token, { httpOnly: true }).sendStatus(200);
+        }
+      });
+    }
+  });
+};
