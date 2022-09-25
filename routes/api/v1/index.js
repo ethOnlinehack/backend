d7d325d2b4a45660ee11bcf2f6dc3f185c349935
Sami <kammounsami77@gmail.com>
2022-09-25 02:41:19 +0100

    Update index.js

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -8,7 +8,7 @@ const transfer = require("./transfer-nft");
 
 router.post("/v1/gamer-sign-in", gamerSignIn);
 router.post("/v1/mint/:walletAddress/:tokenId", mint);
-router.post("/v1/transfer-nft/:walletAddress/:to/:tokenId");
+router.post("/v1/transfer-nft/:walletAddress/:to/:tokenId", transfer);
 router.get("/v1/verify/:walletAddress/:nftId", gamerVerifyNft);
 router.get("/v1/get-all-nfts", getAllNfts);
 router.get("/v1/get-all-nfts/:walletAddress", getAllNftsByGamer);
